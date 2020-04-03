f7942aa0900944b1a5f9b95ed6fed1440d04d8de
Paul Williams <pgwilliams107@gmail.com>
2020-04-03 12:27:11 -0500

    created my launch checklist webpage

--- script.js
+++ script.js
@@ -1,13 +1,77 @@
 // Write your JavaScript code here!
+window.addEventListener("load", function() {
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+      response.json().then( function(json) {
+         const div = document.getElementById("missionTarget");
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+         <h2>Mission Destination</h2>
+         div.innerHTML = `
+            <ol>
+               <li>Name: ${json.name[4]}</li>
+               <li>Diameter: ${json.diameter[4]}</li>
+               <li>Star: ${json.star[4]}</li>
+               <li>Distance from Earth: ${json.distance[4]}</li>
+               <li>Number of Moons: ${json.moons[4]}</li>
+            </ol>
+            <img src="${json.image[4]}">
+         `;
+         
+      });
+   });
+});
+let form = document.querySelector("form");
+form.addEventListener("submit",function(event) {
+   let pilotNameInput = document.querySelector("input[name=pilotName]");
+   let CoPilotNameInput = document.querySelector("input[name=copilotName]");
+   let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+   let cargoMassInput = document.querySelector("input[name=cargoMass]");
+})
+        if (pilotNameInput.value === "" || CoPilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "") {
+         alert("All fields are required!");
+         event.preventDefault();
+      }
+      if (typeof pilotNameInput.value !== 'string' || typeof coPilotNameInput.value !== 'string' || isNaN(fuelLevelInput.value) || isNaN(cargoMassInput.value)) {
+         alert("Incorrect data type!");
+         event.preventDefault();
+      }
+      alert("submit clicked");
+if (fuelLevel.value < 10000) {
+   document.getElementById("faultyItems").style.visibility = "visible";
+   <div id="launchStatusCheck">
+      <h2 style="color: red">id="launchStatus">Shuttle not ready for launch.</h2>
+      <div id="faultyItems">
+         <ol>
+            <li id="pilotStatus">{pilotName.value} is NOT ready for launch</li>
+            <li id="copilotStatus">{copilotName.value} is NOT ready for launch</li>
+            <li id="fuelStatus">There is not enough fuel for the journey.</li>
+            <li id="cargoStatus">Cargo mass low enough for launch</li>
+         </ol>
+      </div>
+   </div>
+} else if (fuelLevel.value > 10000) {
+   document.getElementById("faultyItems").style.visibility = "visible";
+   <div id="launchStatusCheck">
+      <h2 style="color: red">id="launchStatus">Shuttle not ready for launch.</h2>
+      <div id="faultyItems">
+         <ol>
+            <li id="pilotStatus">{pilotName.value} is NOT ready for launch.</li>
+            <li id="copilotStatus">{copilotName.value} is NOT ready for launch.</li>
+            <li id="fuelStatus">There is not enough fuel for the journey.</li>
+            <li id="cargoStatus">There is too much mass for the shuttle to take off.</li>
+         </ol>
+      </div>
+   </div>
+} else {
+   <div id="launchStatusCheck">
+      <h2 style="color: green">id="launchStatus">Shuttle is ready for launch.</h2>
+      <div  id="faultyItems">
+         <ol>
+             <li id="pilotStatus">{pilotName.value} is ready for launch.</li>
+             <li id="copilotStatus">{copilotName.value} is ready for launch.</li>
+             <li id="fuelStatus">Fuel level high enough for launch</li>
+             <li id="cargoStatus">Cargo mass low enough for launch</li>
+         </ol>
+      </div>
+   </div>  
+}
+
