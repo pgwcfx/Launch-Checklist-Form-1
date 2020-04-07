b92951f6f352b9846ca116652d225113aca198c5
Paul Williams <pgwilliams107@gmail.com>
2020-04-06 20:27:27 -0500

    updated my webpage one final time

--- script.js
+++ script.js
@@ -20,6 +20,8 @@ window.addEventListener("load", function() {
    });
    let form = document.querySelector("form");
    form.addEventListener("submit",function(event) {
+      event.preventDefault();
+
       let pilotNameInput = document.querySelector("input[name=pilotName]");
       let CoPilotNameInput = document.querySelector("input[name=copilotName]");
       let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
@@ -40,20 +42,18 @@ window.addEventListener("load", function() {
       }
       if (typeof pilotNameInput.value !== 'string' || typeof coPilotNameInput.value !== 'string' || isNaN(fuelLevelInput.value) || isNaN(cargoMassInput.value)) {
          alert("Incorrect data type!");
-         event.preventDefault();
       }
       alert("submit clicked");
+      document.getElementById("pilotStatus").innerHTML = `Pilot ${pilotNameInput.value} is ready for launch.`;
+      document.getElementById("copilotStatus").innerHTML = `Pilot ${CoPilotNameInput.value} is ready for launch.`;
       if (fuelLevel.value < 10000) {
          document.getElementById("faultyItems").style.visibility = "visible";
-         div.innerHTML = `
-            <h2 style="color: red">Shuttle not ready for launch.</h2>
-               <ol>
-                  <li>${pilotStatus}: ${pilotName.value} is NOT ready for launch</li>
-                  <li>${copilotStatus}: ${copilotName.value} is NOT ready for launch</li>
-                  <li>${fuelStatus}: There is not enough fuel for the journey.</li>
-                  <li>${cargoStatus}: Cargo mass low enough for launch</li>
-               </ol>
-         `;
+         document.getElementById("launchStatus").style.color = "red";
+         document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch.";
+         document.getElementById("pilotStatus").innerHTML = `Pilot ${pilotNameInput.value} is NOT ready for launch.`;
+         document.getElementById("copilotStatus").innerHTML = `Copilot ${CoPilotNameInput.value} is NOT ready for launch.`;
+         document.getElementById("fuelStatus").innerHTML = "There is not enough fuel for the journey.";
+         document.getElementById("cargoStatus").innerHTML = "Cargo mass low enough for launch.";
          /*
          <div id="launchStatusCheck">
             <h2 style="color: red">id="launchStatus">Shuttle not ready for launch.</h2>
@@ -69,15 +69,12 @@ window.addEventListener("load", function() {
          */
       } else if (fuelLevel.value > 10000) {
          document.getElementById("faultyItems").style.visibility = "visible";
-         div.innerHTML = `
-            <h2 style="color: red">Shuttle not ready for launch.</h2>
-               <ol>
-                  <li>${pilotStatus}: ${pilotName.value} is NOT ready for launch</li>
-                  <li>${copilotStatus}: ${copilotName.value} is NOT ready for launch</li>
-                  <li>${fuelStatus}: There is not enough fuel for the journey.</li>
-                  <li>${cargoStatus}: There is too much mass for the shuttle to take off.</li>
-               </ol>
-         `;
+         document.getElementById("launchStatus").style.color = "red";
+         document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch";
+         document.getElementById("pilotStatus").innerHTML = `Pilot ${pilotName.value} is NOT ready for launch.`;
+         document.getElementById("copilotStatus").innerHTML = `Copilot ${copilotName.value} is NOT ready for launch.`;
+         document.getElementById("fuelStatus").innerHTML = "There is not enough fuel for the journey.";
+         document.getElementById("cargoStatus").innerHTML = "There is too much mass for the shuttle to take off.";
          /*
          <div id="launchStatusCheck">
             <h2 style="color: red">id="launchStatus">Shuttle not ready for launch.</h2>
@@ -92,15 +89,12 @@ window.addEventListener("load", function() {
          </div>
          */
       } else {
-         div.innerHTML = `
-            <h2 style="color: green">Shuttle is ready for launch.</h2>
-               <ol>
-                  <li>${pilotStatus}: ${pilotName.value} is ready for launch</li>
-                  <li>${copilotStatus}: ${copilotName.value} is ready for launch</li>
-                  <li>${fuelStatus}: Fuel level high enough for launch</li>
-                  <li>${cargoStatus}: Cargo mass low enough for launch</li>
-               </ol>
-         `;
+         document.getElementById("launchStatus").style.color = "green";
+         document.getElementById("launchStatus").innerHTML = "Shuttle is ready for launch.";
+         document.getElementById("pilotStatus").innerHTML = `Pilot ${pilotName.value} is ready for launch.`;
+         document.getElementById("copilotStatus").innerHTML = `Copilot ${copilotName.value} is ready for launch.`;
+         document.getElementById("fuelStatus").innerHTML = "Fuel level high enough for launch.";
+         document.getElementById("cargoStatus").innerHTML = "Cargo mass low enough for launch.";
          /*
          <div id="launchStatusCheck">
             <h2 style="color: green">id="launchStatus">Shuttle is ready for launch.</h2>
