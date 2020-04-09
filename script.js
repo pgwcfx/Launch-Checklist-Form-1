12e82227dd0556c8b5e8f5d27b1bbbf3e9363ab6
Paul Williams <pgwilliams107@gmail.com>
2020-04-08 21:42:14 -0500

    updated my webpage again

--- script.js
+++ script.js
@@ -26,18 +26,33 @@ window.addEventListener("load", function() {
       let cargoMassInput = document.querySelector("input[name=cargoMass]");
       let fuelUpdate = document.getElementById("fuelStatus");
       let launchUpdate = document.getElementById("launchStatus");
-      let faultyItemsUpdate = document.getElementById("faultyItems");
       let cargoUpdate = document.getElementById("cargoStatus");
       let pilotUpdate = document.getElementById("pilotStatus");
       let copilotUpdate = document.getElementById("copilotStatus");
+      let faultyItemsUpdate = document.getElementById("faultyItems");
       if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "") {
          alert("All fields are required!");
          event.preventDefault();
-      }
-      if (!isNaN(pilotNameInput.value) || !isNaN(copilotNameInput.value) || isNaN(fuelLevelInput.value) || isNaN(cargoMassInput.value)) {
+      } else if (!isNaN(pilotNameInput.value) || !isNaN(copilotNameInput.value) || isNaN(fuelLevelInput.value) || isNaN(cargoMassInput.value)) {
          alert("Incorrect data type!");
+         event.preventDefault();
+      } else {
+         faultyItemsUpdate.style.visibility = "visible";
+         if (fuelLevelInput.value < 10000) {
+            launchUpdate.style.color = "red";
+            launchUpdate.innerHTML = "Shuttle not ready for launch.";
+            fuelUpdate.innerHTML = "There is not enough fuel for the journey.";
+         } 
+         if (cargoMassInput.value > 10000) {
+            launchUpdate.style.color = "red";
+            launchUpdate.innerHTML = "Shuttle not ready for launch";
+            cargoUpdate.innerHTML = "There is too much mass for the shuttle to take off.";
+         } 
+         if (fuelUpdate.value > 10000 && cargoUpdate.value < 10000) {
+            launchUpdate.style.color = "green";
+            launchUpdate.innerHTML = "Shuttle is ready for launch.";
+         }
       }
-      faultyItemsUpdate.style.visibility = "visible";
     /*  pilotUpdate.innerHTML = `Pilot ${pilotNameInput.value} is ready for launch.`;
       copilotUpdate.innerHTML = `Copilot ${copilotNameInput.value} is ready for launch.`;
       if (fuelUpdate.value < 10000) {
